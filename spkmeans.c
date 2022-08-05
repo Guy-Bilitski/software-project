5aa22996b2fadd5215f5767357c682710d1fa972
Sagi Ahrac <sagiahrac@localhost.localdomain>
2022-08-05 12:14:36 +0300

    update

---
+++ spkmeans.c
@@ -0,0 +1,25 @@
+#include <float.h>
+#include <math.h>
+#include <ctype.h>
+
+double gaussian_RBF(PyObject *x1, PyObject *x2);
+double euclidean_distance(PyObject *x1, PyObject *x2);
+
+/*int argc, char **argv*/
+
+int main(){
+
+}
+
+
+
+/*computes w_i in the weighted adjacency matrix*/
+double gaussian_RBF(double *x1, double *x2){
+    
+}
+
+
+/*computes euclidean norm*/
+double euclidean_distance(PyObject *x1, PyObject *x2) {
+
+}
